da09b95762e90b61f9671248dbc2ef8c5ab65fc8
Stephen Alonzo <stephena0228@gmail.com>
2023-03-16 21:31:04 -0700

    updated database config

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'upfront'),
             'username' => env('DB_USERNAME', 'upfront_root'),
-            'password' => env('DB_PASSWORD', 'zYLWG-yJ9<uz.Tb5/'),
+            'password' => env('DB_PASSWORD', 'RpkdbRkUW8>b!Ci7W'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
